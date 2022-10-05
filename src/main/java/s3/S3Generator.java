c7109cd0f2b939ce7c657004b95aaae8f9666271
95Seo <ay9564@naver.com>
2022-10-05 19:22:58 +0900

    Feat : S Client ãBuient Generator 작성

---
+++ src/main/java/s3/S3Generator.java
@@ -0,0 +1,22 @@
+package s3;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+public class S3Generator {
+    public AmazonS3 amazonS3(
+            String accessKey,
+            String secretKey,
+            String region
+    ) {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
+}
